57a8093d703b0b8f857dcba0a7ba75b840aa631e
Ken Chan <chchan585@gmail.com>
2023-01-18 15:04:02 +0800

    extract script and service config file from user_data.sh

---
+++ enable-code-server-service.sh
@@ -0,0 +1,3 @@
+sudo systemctl daemon-reload
+sudo systemctl enable /home/dev/code-server-install/start-code-server.service
+sudo systemctl start start-code-server
---
+++ start-code-server.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+nohup code-server --accept-server-license-terms >> /home/dev/log/code-server.log 2>&1 &
