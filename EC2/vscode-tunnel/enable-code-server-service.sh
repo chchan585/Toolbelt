7cf7fea33058e131f04906c81c1964040d45d299
Ken Chan <chchan585@gmail.com>
2023-01-20 09:14:34 +0800

    Add comment on enable-code-server-service.sh

--- EC2/vscode-tunnel/enable-code-server-service.sh
+++ EC2/vscode-tunnel/enable-code-server-service.sh
@@ -1,3 +1,10 @@
+## Reload systemd manager configuration. 
+## This step is necessary when you add a new service
 sudo systemctl daemon-reload
+
+## register and enable the start-code-server service
+## after this, the instance will start this service on boot
 sudo systemctl enable /home/dev/code-server-install/start-code-server.service
+
+## start the service
 sudo systemctl start start-code-server
