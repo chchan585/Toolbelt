23edbed77728561c8706f5b4b6acb5e5771c7008
Ken Chan <chchan585@gmail.com>
2023-01-20 10:49:55 +0800

    rename tool

--- EC2/vscode-tunnel-helper/user_data.sh
+++ EC2/vscode-tunnel-helper/user_data.sh
@@ -17,15 +17,15 @@ chmod 707 /home/dev/log
 wget -O- https://aka.ms/install-vscode-server/setup.sh | sh
 
 ## create start-code-server.sh
-wget -O /home/dev/code-server-install/start-code-server.sh https://raw.githubusercontent.com/chchan585/Toolbelt/main/EC2/vscode-tunnel/start-code-server.sh
+wget -O /home/dev/code-server-install/start-code-server.sh https://raw.githubusercontent.com/chchan585/Toolbelt/main/EC2/vscode-tunnel-helper/start-code-server.sh
 chmod 745 /home/dev/code-server-install/start-code-server.sh
 
 ## create start-code-server.service
-wget -O /home/dev/code-server-install/start-code-server.service https://raw.githubusercontent.com/chchan585/Toolbelt/main/EC2/vscode-tunnel/start-code-server.service
+wget -O /home/dev/code-server-install/start-code-server.service https://raw.githubusercontent.com/chchan585/Toolbelt/main/EC2/vscode-tunnel-helper/start-code-server.service
 chmod 664 /home/dev/code-server-install/start-code-server.service
 
 ## get enable-code-server-service.sh from github
-wget -O /home/dev/code-server-install/enable-code-server-service.sh https://raw.githubusercontent.com/chchan585/Toolbelt/main/EC2/vscode-tunnel/enable-code-server-service.sh
+wget -O /home/dev/code-server-install/enable-code-server-service.sh https://raw.githubusercontent.com/chchan585/Toolbelt/main/EC2/vscode-tunnel-helper/enable-code-server-service.sh
 chmod 745 /home/dev/code-server-install/enable-code-server-service.sh
 
 ## change back the ownweship to dev
@@ -33,7 +33,7 @@ chown -R dev /home/dev/code-server-install
 chown -R dev /home/dev/log
 
 ## setup guide:
-## 0. Set this in user data field or manually execute: "wget -O- https://raw.githubusercontent.com/chchan585/Toolbelt/main/EC2/vscode-tunnel/user_data.sh | sh"
+## 0. Set this in user data field or manually execute: "wget -O- https://raw.githubusercontent.com/chchan585/Toolbelt/main/EC2/vscode-tunnel-helper/user_data.sh | sh"
 ## 1. run "sudo -i -u dev code-server --accept-server-license-terms", do the initial work then quit
 ## 2. "sudo -i -u dev /home/dev/code-server-install/enable-code-server-service.sh"
 
